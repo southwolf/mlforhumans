bd0ddee2264f08748419e9f0622ebfdc0bc23de8
bdol <bdolmail@gmail.com>
2015-06-09 14:25:20 -0700

    made final project web page

--- colors.js
+++
@@ -1,4 +0,0 @@
-var pos_color = "74, 154, 212";
-var pos_hex = "#4A9AD4";
-var neg_color = "255, 162, 0";
-var neg_hex = "#FFA200";
--- info_box.js
+++
@@ -1,9 +0,0 @@
-function show_info() {
-    document.getElementById('light').style.display='block';
-    document.getElementById('fade').style.display='block';
-}
-
-function hide_info() {
-    document.getElementById('light').style.display='none';
-    document.getElementById('fade').style.display='none';
-}
